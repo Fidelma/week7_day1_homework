c3ac388f79ea4887a1bc7f4edc5601e5105a7ee7
Fidelma <fidelma.beagan@gmail.com>
2019-08-05 17:30:56 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => { new Vue({
+  el: "#app",
+  data: {
+    list: [
+      {name: 'Do Homework', priority: 'high'},
+      {name: 'Grocery shopping', priority: 'low'}
+    ],
+    newItem: '',
+    priority: ''
+  },
+  methods: {
+    saveNewItem: function (e) {
+      e.preventDefault();
+      this.list.push({
+        name: this.newItem,
+        priority: this.priority
+      });
+      this.newItem = '',
+      this.priority = '';
+
+    },
+
+    updatePriority: function (index) {
+      if (this.list[index].priority === 'high') {
+        this.list[index].priority = 'low'
+      } else {
+        this.list[index].priority = 'high'
+      }
+    },
+    
+    deleteItem: function (index) {
+      this.list.splice(this.list[index], 1);
+    }
+  }
+});
+
+});
